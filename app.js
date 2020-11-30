3f333532470e8bee59ac6ddad0003b7032a4a1f7
Gkolman <Gkolman17@gmail.com>
2020-11-30 14:52:22 -0800

    Completed React Components

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// TODO
+
+
+
+class GroceryList extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      inCart: false,
+      lookingAt: false
+    };
+  }
+  onGroceryClick() {
+    this.setState({
+      inCart: !this.state.inCart
+    });
+  }
+
+  onGroceryMouseOver() {
+    this.setState({
+      lookingAt: !this.state.lookingAt
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.inCart ? 'line-through' : 'none',
+      'font-weight': this.state.lookingAt ? 'bold' : 'normal',
+    };
+  return (
+      <li style={style}
+       onClick={this.onGroceryClick.bind(this)}
+       onMouseOver = {this.onGroceryMouseOver.bind(this)}
+       onMouseOut = {this.onGroceryMouseOver.bind(this)} >
+       {this.props.groceries} </li>
+  );
+  }
+};
+
+ var App = () => (
+   <div>
+      <h2>My Grocery List</h2>
+      <ul>
+      <GroceryList groceries = {'milk'} />
+      <GroceryList groceries = {'eggs'} />
+      <GroceryList groceries = {'tea'} />
+      </ul>
+
+   </div>
+);
+
+ReactDOM.render(<App/>, document.getElementById("app"));
+
